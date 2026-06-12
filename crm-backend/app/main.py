f6407771fd81f2fe6b6b229eb498d48cb3adf4b4
Tejasvi Verma <130896989+tejasviverma@users.noreply.github.com>
2026-06-12 16:08:11 +0530

    CORS Middleware

--- crm-backend/app/main.py
+++ crm-backend/app/main.py
@@ -19,10 +19,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://brewmind-dashboard.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(customer_router)
